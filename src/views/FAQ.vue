dde57fe2a49184f3b126883580de1a3b90063653
Ilya Pimenov <ipimenov@gmail.com>
2021-10-14 00:03:08 +0300

    faq & history

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    <h1>FAQ</h1>
-  </div>
+  <v-container>
+    <p class="text-h4">FAQ</p>
+    <v-row>
+      <v-col>
+        <strong>У меня не работает сайт</strong>
+        <div class="text-body-2">
+          Если что-то не работает на ресурсе, пишите на admin@krabr.com.
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <strong>Мне непонятно, что здесь написано</strong>
+        <div class="text-body-2">
+          Если что-то не понятно, загуглите, или почитайте википедию, или, в конце концов, советскую энциклопедию.
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <strong>Всё равно не понятно</strong>
+        <div class="text-body-2">
+          Если все равно не понятно, но очень важное, то пишите на noanswer@krabr.com.
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <strong>Чем могу помочь?</strong>
+        <div class="text-body-2">
+          Если вы хотите помочь проекту: <span class="font-italic">Cбербанк 3456 4578 7545 7543</span>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,7 +1,88 @@
 <template>
-  <div>
-    <h1>History</h1>
-  </div>
+  <v-container>
+    <p class="text-h4">О предыдущих КРаБР</p>
+    <v-timeline>
+      <v-timeline-item
+          color="pink"
+          small
+      >
+        <template v-slot:opposite>
+        <span
+            class="headline font-weight-bold pink--text"
+            v-text="2019"
+        ></span>
+        </template>
+        <div class="py-4">
+          <h2 class="headline font-weight-light mb-4 pink--text">
+            Детство
+          </h2>
+          <div>
+            Когда КРаБР был маленький, он рос на севере в Сибири. Детство его проходило в городе Томск. Там его обучили
+            быть интересным и музыкальным, не стесняться и дарить удовольствие.
+          </div>
+        </div>
+      </v-timeline-item>
+      <v-timeline-item
+          color="orange"
+          small
+      >
+        <template v-slot:opposite>
+        <span
+            class="headline font-weight-bold orange--text"
+            v-text="2020"
+        ></span>
+        </template>
+        <div class="py-4">
+          <h2 class="headline font-weight-light mb-4 orange--text">
+            Юность
+          </h2>
+          <div>
+            Юность КРаБР проходила на удаленке в Москве и МО, там он сдавал ПЦР, вакцинировался и стал более
+            зрелым. Появилась щетина и попробовал алкоголь.
+          </div>
+        </div>
+      </v-timeline-item>
+      <v-timeline-item
+          color="amber"
+          small
+      >
+        <template v-slot:opposite>
+        <span
+            class="headline font-weight-bold amber--text"
+            v-text="2021"
+        ></span>
+        </template>
+        <div class="py-4">
+          <h2 class="headline font-weight-light mb-4 amber--text">
+            Служба в армии
+          </h2>
+          <div>
+            Службу в ВС РФ КРаБР проходит в настоящий момент в ВДВ в городе Рязань. "Расплескалась синева,
+            расплескалась..."
+          </div>
+        </div>
+      </v-timeline-item>
+      <v-timeline-item
+          color="cyan"
+          small
+      >
+        <template v-slot:opposite>
+        <span
+            class="headline font-weight-bold cyan--text"
+            v-text="2022"
+        ></span>
+        </template>
+        <div class="py-4">
+          <h2 class="headline font-weight-light mb-4 cyan--text">
+            Дембель!
+          </h2>
+          <div>
+            А где будет после дембеля, где он женится и где появятся маленькие крабрята - решать вам. Голосуйте!
+          </div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
 
 <script>
